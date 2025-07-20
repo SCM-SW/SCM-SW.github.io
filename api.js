ca938b446cfb2c061a15a82b581d9d2e04b44396
SCM-SW <aghiesab@outlook.com>
2025-07-20 15:26:14 +0300

    Update api.js

--- api.js
+++ api.js
@@ -1,17 +1,1 @@
-// api.js
-export async function getPlaceId(url) {
-  try {
-    const response = await fetch(url);
-    if (!response.ok) throw new Error('Network error');
-    const text = await response.text();
-    const placeId = text.trim();
-
-    if (/^\d+$/.test(placeId)) {
-      return { status: 'online', placeId };
-    } else {
-      return { status: 'offline' };
-    }
-  } catch (error) {
-    return { status: 'error', error };
-  }
-}
+return {status: 'online'};
